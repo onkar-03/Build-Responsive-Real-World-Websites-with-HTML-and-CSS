2b63ed25ca459fab4b94e6c874775a9c436ffdc7
Onkar Patel <onkarpatel10@gmail.com>
2024-02-25 21:46:18 +0530

    Implementing Smooth Scrolling using Js !!

--- 06 - OmniFood Desktop/Js/script.js
+++ 06 - OmniFood Desktop/Js/script.js
@@ -10,3 +10,30 @@ const header = document.querySelector('.header');
 button.addEventListener('click', function () {
   header.classList.toggle('nav-open');
 });
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      header.classList.toggle('nav-open');
+  });
+});
