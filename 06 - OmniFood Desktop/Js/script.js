0b4c5a5c090913a9b6b459d6c425b548aa80a70e
Onkar Patel <onkarpatel10@gmail.com>
2024-02-25 21:23:21 +0530

    Added Toggle Navigation on Button Click with JavaScript for mobile layout

--- 06 - OmniFood Desktop/Js/script.js
+++ 06 - OmniFood Desktop/Js/script.js
@@ -1,5 +1,12 @@
 // Dynamic Year update in HTML using JavaScript in footer section
-
 const year = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 year.textContent = currentYear;
+
+// Toggle menu for Mobile Navigation
+const button = document.querySelector('.btn-mobile-nav');
+const header = document.querySelector('.header');
+
+button.addEventListener('click', function () {
+  header.classList.toggle('nav-open');
+});
