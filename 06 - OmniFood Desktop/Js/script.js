2e70440b05c3e9d5480351ebb768a6c4eac519cd
Onkar Patel <onkarpatel10@gmail.com>
2024-02-26 00:43:57 +0530

    Implementing Sticky Navigation to the WebPage :
    - Added sticky navigation on scroll
    
    - Sticky navigation activates when hero section is out of viewport
    
    - Utilized Intersection Observer API for efficient detection
    
    - 'sticky' class toggled on body element for styling
    
    - Set threshold to 0 for immediate activation
    
    - Applied root margin of -80px for smooth transition
    
    Enhances user experience by maintaining navigation visibility

--- 06 - OmniFood Desktop/Js/script.js
+++ 06 - OmniFood Desktop/Js/script.js
@@ -37,3 +37,37 @@ allLinks.forEach(function (link) {
       header.classList.toggle('nav-open');
   });
 });
+
+// Sticky Navigation using Js
+// We want the navigation to get sticky when the hero-section is out of viewport
+const sectionHero = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    // Collecting the first entry from array of entries which corresponds to each threshold value
+    const ent = entries[0];
+
+    // Add sticky nav as soon as the section ends
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    }
+
+    // Remove sticky nav as soon as we get back up
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    // viewing the stick nav In the Viewport -> root:null;
+    root: null,
+
+    //Fire event the sticky nav as soon as there is 0% of the hero-section is inside of the  viewport
+    threshold: 0,
+
+    // To load the sticky nav as soon as there is only 80px left for hte hero section to finish completely
+    rootMargin: '-80px',
+  },
+);
+
+// Calling Function
+obs.observe(sectionHero);
